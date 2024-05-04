4f43376dfdcf592d2d3d55fb542376d53abfd566
GabLocal <gabrielborcena@gmail.com>
2024-05-04 21:32:08 +0800

    modify vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/TaskManagement/' // This should be the name of your GitHub repository
+  base: '/TaskManagement-v2/' // This should be the name of your GitHub repository
 })  
